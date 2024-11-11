99788165d5fd465318619b963b324de80833e116
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-11 14:10:27 +0300

    firebase start

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAn2rawjjnzjo3ZYZPBuYgJT33d32-RuQw",
+  authDomain: "chat-app-970c5.firebaseapp.com",
+  projectId: "chat-app-970c5",
+  storageBucket: "chat-app-970c5.firebasestorage.app",
+  messagingSenderId: "469614275644",
+  appId: "1:469614275644:web:2aa0581fd91e8d7e3e2739"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
