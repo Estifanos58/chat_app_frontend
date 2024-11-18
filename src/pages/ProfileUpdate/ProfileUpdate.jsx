debc002f6b57b5c9f512129fb6228d0c85a116ae
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-18 11:23:32 +0300

    userData

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,57 @@
 import { useState } from 'react'
 import assets from '../../assets/assets'
 import './ProfileUpdate.css'
+import axios from 'axios'
 
 function ProfileUpdate() {
   const [image, setImage] = useState(false)
+  const [prevImage, setPrevImage] = useState('');
+
+  const [formData, setFormData] = useState({
+    bio: "",
+    avatar: "",
+    name: ""
+  })
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+    try {
+      
+    } catch (error) {
+      
+    }
+    console.log(formData);
+  }
+
+  const hadleImageSubmit = (e) => {
+    const img = e.target.files[0];
+    setImage(e.target.files[0])
+    const reader = new FileReader();
+
+
+    // const file = event.target.files[0];  
+    // const reader = new FileReader();  
+
+    reader.onloadend = () => {  
+      // The result will be base64 encoded  
+      // setBase64Image(reader.result);  
+      setFormData({
+        ...formData,
+        avatar: reader.result
+      })
+    };  
+
+    if (img) {  
+      reader.readAsDataURL(img);  
+    } 
+  }
 
   return (
     <div className='profile'>
@@ -11,13 +59,13 @@ function ProfileUpdate() {
         <form>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
-            <input onChange={(e)=> setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <input onChange={hadleImageSubmit} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
             <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
             upload profile image
           </label>
-          <input type="text" placeholder='Your name' required />
-          <textarea placeholder='Write profile bio' required id=""></textarea>
-          <button type="submit">Save</button>
+          <input value={formData.name} onChange={handleChange} name='name' type="text" placeholder='Your name' required />
+          <textarea value={formData.bio} onChange={handleChange} name='bio' placeholder='Write profile bio' required id=""></textarea>
+          <button type="submit" onClick={handleSubmit}>Save</button>
         </form>
         <img className='profile-pic' src={image? URL.createObjectURL(image): assets.logo_icon} alt="" />
       </div>
